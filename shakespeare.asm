87146dd2b3dbea01e7d38fd7a3728e57ce1d942e
tjetj <168307178+tjetj@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    htimeina893nff

---
+++ shakespeare.asm
@@ -0,0 +1,9 @@
+POSTHUMUS LEONATUS
+
+    Not any, but abide the change of time,
+    Quake in the present winter's state and wish
+    That warmer days would come: in these sear'd hopes,
+    I barely gratify your love; they failing,
+    I must die much your debtor.
+
+; Enough commas to pass as assembly code
